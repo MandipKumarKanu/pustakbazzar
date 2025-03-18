f9ffa722ef5173959165124df5c34a5d00ea7cf6
aadik6 <aadarshkumarman@gmail.com>
2025-03-18 22:10:37 +0545

    port check

--- backend/index.js
+++ backend/index.js
@@ -34,7 +34,7 @@ app.use("/api/cart", cartRoute);
 app.use("/api/order", orderRoutes);
 app.use("/api/donation", donationRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
